d84802f07994ed27d3ea028cf2dac9cea3af14f4
naradbx-dsa-blaze <naradbx-dsa-blaze>
2024-06-08 19:28:29 +0000

    commit

--- 04_mlflow_logging.py
+++ 04_mlflow_logging.py
@@ -7,31 +7,31 @@ dbutils.library.restartPython()
 
 # COMMAND ----------
 
-import mlflow
-import transformers
-import torch
-import mlflow
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer
-)
+# import mlflow
+# import transformers
+# import torch
+# import mlflow
+# from transformers import (
+#     AutoModelForCausalLM,
+#     AutoTokenizer
+# )
 
-model = AutoModelForCausalLM.from_pretrained("geekdom/medmistral-7b")
-tokenizer = AutoTokenizer.from_pretrained("geekdom/medmistral-7b")
-registered_model_name = "ang_nara_catalog.llmops.medbrief-7b"
-model_config = {
-    "max_length": 800,
-}
+# model = AutoModelForCausalLM.from_pretrained("geekdom/medmistral-7b")
+# tokenizer = AutoTokenizer.from_pretrained("geekdom/medmistral-7b")
+# registered_model_name = "ang_nara_catalog.llmops.medbrief-7b"
+# model_config = {
+#     "max_length": 800,
+# }
 
-with mlflow.start_run():
-    components = {
-        "model": model,
-        "tokenizer": tokenizer,
-    }
-    mlflow.transformers.log_model(
-        transformers_model=components,
-        model_config=model_config,
-        artifact_path="model",
-        task="llm/v1/completions",
-        registered_model_name=registered_model_name
-    )
+# with mlflow.start_run():
+#     components = {
+#         "model": model,
+#         "tokenizer": tokenizer,
+#     }
+#     mlflow.transformers.log_model(
+#         transformers_model=components,
+#         model_config=model_config,
+#         artifact_path="model",
+#         task="llm/v1/completions",
+#         registered_model_name=registered_model_name
+#     )
