98768ec4bd05e7bb4c4beb2d27efb7aedbe0b9fe
naradbx-dsa-blaze <naradbx-dsa-blaze>
2024-06-16 19:32:04 +0000

    commit

--- 09_avg_rouge_calc.py
+++ 09_avg_rouge_calc.py
@@ -1,2 +1,19 @@
 # Databricks notebook source
+df = spark.read.table("ang_nara_catalog.llmops.ft_mistral7b_endpoint_processed_profile_metrics")
 
+# COMMAND ----------
+
+#define baseline rouge
+baseline = 0.20
+
+#compute average
+average = df.agg({"avg_rouge": "avg"}).collect()[0][0]
+
+
+# COMMAND ----------
+
+dbutils.jobs.taskValues.set(key = "rouge", value = average)
+
+# COMMAND ----------
+
+dbutils.jobs.taskValues.get(taskKey="09_avg_rouge_calc", key="rouge", default=average, debugValue=average)
---
+++ 10_copy_to_prod_catalog.py
@@ -0,0 +1,29 @@
+# Databricks notebook source
+import mlflow
+from mlflow.exceptions import MlflowException
+
+mlflow.set_registry_uri("databricks-uc")
+
+client = mlflow.tracking.MlflowClient()
+src_model_name = "ang_nara_catalog.llmops.medbrief-7b"
+src_model_version = "1"
+src_model_uri = f"models:/{src_model_name}/{src_model_version}"
+dst_model_name = "nara_catalog.llmops.medbrief-7b"
+
+# Check if the destination model version already exists
+def model_version_exists(client, model_name, src_version):
+    versions = client.search_model_versions(f"name='{model_name}'")
+    for version in versions:
+        if version.version == src_version:
+            return True
+    return False
+
+if not model_version_exists(client, dst_model_name, src_model_version):
+    try:
+        copied_model_version = client.copy_model_version(src_model_uri, dst_model_name)
+        print(f"Model version copied successfully: {copied_model_version.version}")
+    except MlflowException as e:
+        print(f"Failed to copy model version: {e}")
+else:
+    print("Model version already exists in the destination. Skipping copy.")
+
