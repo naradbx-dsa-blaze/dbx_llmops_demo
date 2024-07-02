c9acf5c76ead8a693143c05d6beddd47d454ff9c
naradbx-dsa-blaze <naradbx-dsa-blaze>
2024-07-02 02:58:32 +0000

    commit

--- 02_finetuning.py
+++ 02_finetuning.py
@@ -1,34 +1,35 @@
 # Databricks notebook source
-# Let's start by installing our products
-%pip install databricks-genai==1.0.2
-%pip install databricks-sdk==0.27.1
-%pip install "mlflow==2.12.2"
-dbutils.library.restartPython()
+# # Let's start by installing our products
+# %pip install databricks-genai==1.0.2
+# %pip install databricks-sdk==0.27.1
+# %pip install "mlflow==2.12.2"
+# dbutils.library.restartPython()
 
 # COMMAND ----------
 
-from databricks.model_training import foundation_model as fm
-#Return the current cluster id to use to read the dataset and send it to the fine tuning cluster. See https://docs.databricks.com/en/large-language-models/foundation-model-training/create-fine-tune-run.html#cluster-id
-def get_current_cluster_id():
-  import json
-  return json.loads(dbutils.notebook.entry_point.getDbutils().notebook().getContext().safeToJson())['attributes']['clusterId']
+# from databricks.model_training import foundation_model as fm
+# #Return the current cluster id to use to read the dataset and send it to the fine tuning cluster. See https://docs.databricks.com/en/large-language-models/foundation-model-training/create-fine-tune-run.html#cluster-id
+# def get_current_cluster_id():
+#   import json
+#   return json.loads(dbutils.notebook.entry_point.getDbutils().notebook().getContext().safeToJson())['attributes']['clusterId']
 
 
-#Let's clean the model name
-registered_model_name = "ang_nara_catalog.llmops.ift-medbrief8b"
+# #Let's clean the model name
+# registered_model_name = "ang_nara_catalog.llmops.ift-medbrief8b"
 
-run = fm.create(
-    data_prep_cluster_id=get_current_cluster_id(),  # required if you are using delta tables as training data source. This is the cluster id that we want to use for our data prep job.
-    model="meta-llama/Meta-Llama-3-8B-Instruct",  # Here we define what model we used as our baseline
-    train_data_path="ang_nara_catalog.llmops.create_train_data",
-    task_type="INSTRUCTION_FINETUNE",  # Change task_type="INSTRUCTION_FINETUNE" if you are using the fine-tuning API for completion.
-    register_to=registered_model_name,
-    training_duration="20ep", #only 20 epoch to accelerate the demo. Check the mlflow experiment metrics to see if you should increase this number
-    learning_rate="5e-7",
-)
+# run = fm.create(
+#     data_prep_cluster_id=get_current_cluster_id(),  # required if you are using delta tables as training data source. This is the cluster id that we want to use for our data prep job.
+#     model="meta-llama/Meta-Llama-3-8B-Instruct",  # Here we define what model we used as our baseline
+#     train_data_path="ang_nara_catalog.llmops.create_train_data",
+#     eval_data_path="ang_nara_catalog.llmops.create_test_data",
+#     task_type="INSTRUCTION_FINETUNE",  # Change task_type="INSTRUCTION_FINETUNE" if you are using the fine-tuning API for completion.
+#     register_to=registered_model_name,
+#     training_duration="15ep", #only 15 epoch to accelerate the demo. Check the mlflow experiment metrics to see if you should increase this number
+#     learning_rate="5e-7",
+# )
 
-print(run)
+# print(run)
 
 # COMMAND ----------
 
-display(run.get_events())
+# display(run.get_events())
--- 12_gradio_app.py
+++ 12_gradio_app.py
@@ -11,20 +11,14 @@ import re
 class MedicalHistorySummarizer:
     def __init__(self):
         self.deploy_client = mlflow.deployments.get_deploy_client("databricks")
-
-    def filter_incomplete_sentence(self, text):
-        pattern = r'(?:[^.!?]+(?:[.!?](?=\s|$))+\s?)'
-        filtered_sentence = ''.join(re.findall(pattern, text))
-        return filtered_sentence.strip()
-
+        
     def summarize_medical_history(self, prompt, client_request_id):
         # Define input for the model
         input_data = {"prompt": prompt, "client_request_id": client_request_id}
         # Make prediction
         response = self.deploy_client.predict(endpoint="ift-medbrief8b-endpoint ", inputs=input_data)
         # Extract and return the response
-        get_text = response['choices'][0]['text']
-        summary = self.filter_incomplete_sentence(get_text)
+        summary = response['choices'][0]['text']
         return summary
 
 # Create an instance of the summarizer
