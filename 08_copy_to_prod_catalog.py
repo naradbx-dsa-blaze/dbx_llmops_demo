924fbb19f17a194d0cacb93f7a7a8258e7a3d054
naradbx-dsa-blaze <naradbx-dsa-blaze>
2024-07-04 19:08:57 +0000

    commit

--- 08_copy_to_prod_catalog.py
+++ 08_copy_to_prod_catalog.py
@@ -5,10 +5,10 @@ from mlflow.exceptions import MlflowException
 mlflow.set_registry_uri("databricks-uc")
 
 client = mlflow.tracking.MlflowClient()
-src_model_name = "ang_nara_catalog.llmops.medbrief-7b"
-src_model_version = "1"
+src_model_name = "ang_nara_catalog.llmops.ift-medbrief8b"
+src_model_version = "2"
 src_model_uri = f"models:/{src_model_name}/{src_model_version}"
-dst_model_name = "nara_catalog.llmops.medbrief-7b"
+dst_model_name = "nara_catalog.llmops.ift-medbrief8b-prod"
 
 # Check if the destination model version already exists
 def model_version_exists(client, model_name, src_version):
