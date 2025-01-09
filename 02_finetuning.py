e57e3351dbaf9e76bd15f83b3595e14e5d9947a2
naradbx-dsa-blaze <naradbx-dsa-blaze>
2025-01-09 22:37:34 +0000

    commit

--- 02_finetuning.py
+++ 02_finetuning.py
@@ -21,7 +21,7 @@ run = fm.create(
     data_prep_cluster_id=get_current_cluster_id(),  # required if you are using delta tables as training data source. This is the cluster id that we want to use for our data prep job.
     model="meta-llama/Meta-Llama-3-8B-Instruct",  # Here we define what model we used as our baseline
     train_data_path="nara_catalog.ds_demos.train_clinical_data",
-    eval_data_path="nara_catalog.ds_demos.test_clinical_dataa",
+    eval_data_path="nara_catalog.ds_demos.test_clinical_data",
     task_type="INSTRUCTION_FINETUNE",  # Change task_type="INSTRUCTION_FINETUNE" if you are using the fine-tuning API for completion.
     register_to=registered_model_name,
     training_duration="5ep", #only 5 epoch to accelerate the demo. Check the mlflow experiment metrics to see if you should increase this number
